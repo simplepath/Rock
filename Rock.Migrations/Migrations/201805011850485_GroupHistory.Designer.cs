5a284221933a8f3456e05de52df6f0494356f94d
Mike Peterson <mike@sparkabilitygroup.com>
2018-05-01 12:16:43 -0700

    - Rebuild GroupHistory migration after merge from develop

--- Rock.Migrations/Migrations/201805011850485_GroupHistory.Designer.cs
+++ Rock.Migrations/Migrations/201805011850485_GroupHistory.Designer.cs
@@ -13,7 +13,7 @@ namespace Rock.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201804110008230_GroupHistory"; }
+            get { return "201805011850485_GroupHistory"; }
         }
         
         string IMigrationMetadata.Source
