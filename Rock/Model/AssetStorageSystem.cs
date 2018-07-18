ee60314d4bfd3260080a8e3000e90ee6330f817a
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-07-18 16:37:28 -0700

    - Migration to create AssetStorageSystem table.

--- Rock/Model/AssetStorageSystem.cs
+++ Rock/Model/AssetStorageSystem.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.ModelConfiguration;
 using System.Runtime.Serialization;
 
