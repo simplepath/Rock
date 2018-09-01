797d55a5d0359aa1ed3400ee31089eb352189eca
Mike Peterson <mike@sparkabilitygroup.com>
2018-08-31 18:24:18 -0700

    - Started on updates to AssetFieldType edit button so that it show File selection, icon, etc

--- Rock/Field/Types/AssetFieldType.cs
+++ Rock/Field/Types/AssetFieldType.cs
@@ -16,23 +16,67 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Web.UI;
+using Newtonsoft.Json;
 
 using Rock.Data;
-using Rock.Web.UI.Controls;
-using Newtonsoft.Json;
 using Rock.Model;
+using Rock.Web.UI.Controls;
 
 namespace Rock.Field.Types
 {
-    class AssetFieldType : FieldType
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Rock.Field.FieldType" />
+    public class AssetFieldType : FieldType
     {
-        private string _pickerButtonTemplate = @"<div class=""imageupload-thumbnail-image"" style=""height:100px; width:100px; background-image:url(/Assets/Images/no-picture.svg); background-size:cover; background-position:50%""></div>
-                <div class=""imageupload-group imageupload-dropzone""><span><i class=""fa fa-globe-americas""></i>{0}</span></div>";
+        /// <summary>
+        /// The image icon URL no picture
+        /// </summary>
+        private string imageIconUrlNoPicture = "/Assets/Images/no-picture.svg";
 
+        /// <summary>
+        /// The picker button template
+        /// </summary>
+        /// <param name="imageIconUrl">The image icon URL.</param>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns></returns>
+        private string GetPickerButtonTemplate( string imageIconUrl, string fileName )
+        {
+            string fileLinkInfo = null;
+            string fileRemoveHtml = null;
+            if ( fileName.IsNotNullOrWhiteSpace() )
+            {
+                fileLinkInfo = $"<span class='file-link'>{fileName}</span>";
+                fileRemoveHtml = $"<div class='fileupload-remove>#TODO#<i class='fa fa-times'></i></div>";
+            }
+
+            return $@"
+<div class='imageupload-group'>
+    <div class='imageupload-thumbnail-image' style='height:100px; width:100px; background-image:url({imageIconUrl}); background-size:cover; background-position:50%'>
+      {fileLinkInfo}
+      {fileRemoveHtml}
+    </div>
+    <div class='imageupload-dropzone'>
+        <span>
+            <i class=""fa fa-globe-americas""></i>
+            Browse
+        </span>
+    </div>
+</div>
+";
+        }
+
+        /// <summary>
+        /// Creates the control(s) necessary for prompting user for a new value
+        /// </summary>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="id">The id.</param>
+        /// <returns>
+        /// The control
+        /// </returns>
         public override Control EditControl( Dictionary<string, ConfigurationValue> configurationValues, string id )
         {
             var pickerControl = new ItemFromBlockPicker
@@ -43,7 +87,7 @@ namespace Rock.Field.Types
                 CssClass = "btn btn-xs btn-default",
                 ModalSaveButtonText = "Select",
                 ButtonTextTemplate = "Browse",
-                PickerButtonTemplate = string.Format( _pickerButtonTemplate, "Browse" )
+                PickerButtonTemplate = GetPickerButtonTemplate( imageIconUrlNoPicture, null )
             };
 
             pickerControl.SelectItem += AssetBrowser_SelectItem;
@@ -51,6 +95,12 @@ namespace Rock.Field.Types
             return pickerControl;
         }
 
+        /// <summary>
+        /// Reads new values entered by the user for the field
+        /// </summary>
+        /// <param name="control">Parent control that controls were added to in the CreateEditControl() method</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <returns></returns>
         public override string GetEditValue( Control control, Dictionary<string, ConfigurationValue> configurationValues )
         {
             var picker = ( ItemFromBlockPicker ) control;
@@ -62,12 +112,19 @@ namespace Rock.Field.Types
             return string.Empty;
         }
 
+        /// <summary>
+        /// Sets the value.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="value">The value.</param>
         public override void SetEditValue( Control control, Dictionary<string, ConfigurationValue> configurationValues, string value )
         {
             var picker = ( ItemFromBlockPicker ) control;
             if ( picker != null )
             {
                 picker.SelectedValue = value;
+                UpdatePickerHtml( picker );
             }
         }
 
@@ -82,13 +139,12 @@ namespace Rock.Field.Types
         /// <returns></returns>
         public override string FormatValue( Control parentControl, string value, Dictionary<string, ConfigurationValue> configurationValues, bool condensed )
         {
-            if ( value.IsNullOrWhiteSpace() )
+            Storage.AssetStorage.Asset asset = GetAssetInfoFromValue( value );
+
+            if ( asset == null )
             {
                 return string.Empty;
             }
-
-            var asset = JsonConvert.DeserializeObject<Storage.AssetStorage.Asset>( value );
-            asset.Type = Storage.AssetStorage.AssetType.File;
 
             AssetStorageSystem assetStorageSystem = new AssetStorageSystem();
             int? assetStorageId = asset.AssetStorageSystemId;
@@ -107,10 +163,53 @@ namespace Rock.Field.Types
             return uri;
         }
 
+        /// <summary>
+        /// Gets the asset information from value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        private static Storage.AssetStorage.Asset GetAssetInfoFromValue( string value )
+        {
+            Storage.AssetStorage.Asset asset = null;
+
+            if ( !value.IsNullOrWhiteSpace() )
+            {
+                asset = JsonConvert.DeserializeObject<Storage.AssetStorage.Asset>( value );
+                asset.Type = Storage.AssetStorage.AssetType.File;
+            }
+
+            return asset;
+        }
+
+        /// <summary>
+        /// Handles the SelectItem event of the AssetBrowser control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void AssetBrowser_SelectItem( object sender, EventArgs e )
         {
-            //var picker = ( ItemFromBlockPicker) sender;
-            //picker.PickerButtonTemplate = string.Format( _pickerButtonTemplate, picker.SelectedValue );
+            var picker = ( ItemFromBlockPicker ) sender;
+            UpdatePickerHtml( picker );
+        }
+
+        /// <summary>
+        /// Updates the picker HTML.
+        /// </summary>
+        /// <param name="picker">The picker.</param>
+        private void UpdatePickerHtml( ItemFromBlockPicker picker )
+        {
+            Storage.AssetStorage.Asset asset = GetAssetInfoFromValue( picker.SelectedValue );
+            if ( asset != null )
+            {
+                var fileName = Path.GetFileName( asset.Key );
+                var fileTypeExtension = System.IO.Path.GetExtension( asset.Key ).Replace( ".", string.Empty );
+                var imageIconUrl = $"/Assets/Icons/FileTypes/{fileTypeExtension}.png";
+                picker.PickerButtonTemplate = GetPickerButtonTemplate( imageIconUrl, fileName );
+            }
+            else
+            {
+                picker.PickerButtonTemplate = GetPickerButtonTemplate( imageIconUrlNoPicture, null );
+            }
         }
     }
 }
