aee5826dd595dcdd617bc01305a26e52cff13817
Mike Peterson <mike@sparkabilitygroup.com>
2018-10-15 17:31:07 -0700

    - Added Tool_CheckInWelcomeRefreshIntervalTo9999.sql

--- Dev Tools/Sql/Tool_CheckInIdleRedirectTo9999.sql
+++ Dev Tools/Sql/Tool_CheckInIdleRedirectTo9999.sql
@@ -1,3 +1,5 @@
+/* NOTE: If you also want the Welcome Page to wait longer, run Tool_CheckInWelcomeRefreshIntervalTo9999.sql */
+
 DECLARE @IdleSecondsGuidAttended UNIQUEIDENTIFIER = 'A7F99980-BED4-4A80-AB83-DDAB5C7D7AAD'
     ,@IdleSecondsGuidCore UNIQUEIDENTIFIER = '1CAC7B16-041A-4F40-8AEE-A39DFA076C14'
 
