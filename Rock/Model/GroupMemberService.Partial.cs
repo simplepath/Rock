simplepath/Rock

commit 7bc0ee612fa9cfe05f8e3dd493fe2b0abbc75540

- Updated performance of GroupMemberService.CreateKnownRelationship by avoiding loading some unnecessary stuff from the database

Rock/Model/GroupMemberService.Partial.cs
@@ -554,9 +554,9 @@ namespace Rock.Model
             var rockContext = this.Context as RockContext;
 
             var knownRelationshipGroupType = GroupTypeCache.Get( Rock.SystemGuid.GroupType.GROUPTYPE_KNOWN_RELATIONSHIPS );
-            var ownerRole = knownRelationshipGroupType.Roles.FirstOrDefault( r => r.Guid.Equals( Rock.SystemGuid.GroupRole.GROUPROLE_KNOWN_RELATIONSHIPS_OWNER.AsGuid() ) );
+            int? ownerRoleId = knownRelationshipGroupType.Roles.FirstOrDefault( r => r.Guid.Equals( Rock.SystemGuid.GroupRole.GROUPROLE_KNOWN_RELATIONSHIPS_OWNER.AsGuid() ) )?.Id;
             var relationshipRole = knownRelationshipGroupType.Roles.FirstOrDefault( r => r.Id == relationshipRoleId );
-            if ( ownerRole == null )
+            if ( ownerRoleId == null )
             {
                 throw new Exception( "Unable to find known relationships owner role" );
             }
@@ -566,40 +566,41 @@ namespace Rock.Model
                 throw new Exception( "Specified relationshipRoleId is not a known relationships role" );
             }
 
-            var knownRelationshipGroup = groupMemberService.Queryable(true)
+            int? knownRelationshipGroupId = groupMemberService.Queryable(true)
                 .Where( m =>
                     m.PersonId == personId &&
-                    m.GroupRole.Guid.Equals( ownerRole.Guid ) )
-                .Select( m => m.Group )
+                    m.GroupRoleId == ownerRoleId.Value )
+                .Select( m => m.GroupId )
                 .FirstOrDefault();
 
             // Create known relationship group if doesn't exist
-            if ( knownRelationshipGroup == null )
+            if ( !knownRelationshipGroupId.HasValue )
             {
                 var groupMember = new GroupMember();
                 groupMember.PersonId = personId;
-                groupMember.GroupRoleId = ownerRole.Id;
+                groupMember.GroupRoleId = ownerRoleId.Value;
 
-                knownRelationshipGroup = new Group();
+                var knownRelationshipGroup = new Group();
                 knownRelationshipGroup.Name = knownRelationshipGroupType.Name;
                 knownRelationshipGroup.GroupTypeId = knownRelationshipGroupType.Id;
                 knownRelationshipGroup.Members.Add( groupMember );
 
                 new GroupService( rockContext ).Add( knownRelationshipGroup );
                 rockContext.SaveChanges();
+                knownRelationshipGroupId = knownRelationshipGroup.Id;
             }
 
             // Add relationships
             var relationshipMember = groupMemberService.Queryable(true)
                 .FirstOrDefault( m =>
-                    m.GroupId == knownRelationshipGroup.Id &&
+                    m.GroupId == knownRelationshipGroupId.Value &&
                     m.PersonId == relatedPersonId &&
                     m.GroupRoleId == relationshipRoleId );
 
             if ( relationshipMember == null )
             {
                 relationshipMember = new GroupMember();
-                relationshipMember.GroupId = knownRelationshipGroup.Id;
+                relationshipMember.GroupId = knownRelationshipGroupId.Value;
                 relationshipMember.PersonId = relatedPersonId;
                 relationshipMember.GroupRoleId = relationshipRoleId;
                 groupMemberService.Add( relationshipMember );

commit d7648d9da2d58af8f156b44def22ee751e604cd5

- Fix CreateKnownRelationship causing a FK exception (introduced with 7bc0ee612fa9cfe05f8e3dd493fe2b0abbc75540)

Rock/Model/GroupMemberService.Partial.cs
@@ -566,11 +566,11 @@ namespace Rock.Model
                 throw new Exception( "Specified relationshipRoleId is not a known relationships role" );
             }
 
-            int? knownRelationshipGroupId = groupMemberService.Queryable(true)
+            int? knownRelationshipGroupId = groupMemberService.Queryable( true )
                 .Where( m =>
                     m.PersonId == personId &&
                     m.GroupRoleId == ownerRoleId.Value )
-                .Select( m => m.GroupId )
+                .Select( m => ( int? ) m.GroupId )
                 .FirstOrDefault();
 
             // Create known relationship group if doesn't exist