71d375dfe48484e6aa49426a0364b06febe28a41
Mike Peterson <mike@sparkabilitygroup.com>
2018-11-06 15:54:41 -0700

    - Update AttributeValue so that ValueAsDateTime and ValueAsBoolean have 'internal set;' instead of 'private set'. This will allow rock core to set these in cases where an instance of AttributeValue is needed to do in-memory expression filters on Attributevalue.

--- Rock/Model/AttributeValue.cs
+++ Rock/Model/AttributeValue.cs
@@ -152,7 +152,7 @@ namespace Rock.Model
         [DataMember]
         [DatabaseGenerated( DatabaseGeneratedOption.Computed )]
         [LavaIgnore]
-        public DateTime? ValueAsDateTime { get; private set; }
+        public DateTime? ValueAsDateTime { get; internal set; }
 
         /// <summary>
         /// Gets the value as boolean (computed column)
@@ -163,7 +163,7 @@ namespace Rock.Model
         [DataMember]
         [DatabaseGenerated( DatabaseGeneratedOption.Computed )]
         [LavaIgnore]
-        public bool? ValueAsBoolean { get; private set; }
+        public bool? ValueAsBoolean { get; internal set; }
 
         /// <summary>
         /// Gets a person alias guid value as a PersonId (ComputedColumn).
